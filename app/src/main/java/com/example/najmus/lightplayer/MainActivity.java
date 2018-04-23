5e35c8d835fcc61d1fd49249643cb12032ec153c
NAJMUS SEEMAB <najmus.seemab@gmail.co>
2018-04-23 16:02:52 +0530

    test

---
+++ app/src/main/java/com/example/najmus/lightplayer/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.najmus.lightplayer;
+
+import android.app.Service;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    SensorManager sensorManager;
+    Sensor sensor;
+    boolean isRunning = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_main );
+        sensorManager = (SensorManager) getSystemService( Service.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+    }
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    protected void onPause() {
+        super.onPause ();
+
+        sensorManager.unregisterListener ( this );
+
+    }
+
+    MediaPlayer mp;
+
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.values[0] > 20 && isRunning == false) {
+            isRunning = true;
+            try {
+                mp.setDataSource ( "http://WWW.hungama.com/album/baaghi-2/33939236/" );
+                mp.prepare ();
+                mp.start ();
+            } catch (Exception ex) {
+                //play the music
+            }
+
+
+        }
+
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
